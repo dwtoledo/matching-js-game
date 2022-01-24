c71fa269b14313d133cae9d633b3f06c54f35f34
Douglas Toledo <dwtoledo@outlook.com>
2022-01-24 14:08:21 -0300

    add PressStart2P-Regular into the project and create PlayerName component

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 
 import CardGame from "./src/components/CardGame";
+import PlayerName from "./src/components/PlayerName";
+
+const numberOfCards = 6;
 
 const $root = document.getElementById("root");
-const $element = CardGame();
+const $cardGameElement = CardGame();
+const $playerOneElement = PlayerName("Player1");
+const $playerTwoElement = PlayerName("Player2");
 
-$root.insertAdjacentHTML("beforeend", $element);
+$root.insertAdjacentHTML("afterbegin", $playerTwoElement);
+$root.insertAdjacentHTML("afterbegin", $playerOneElement);
+$root.insertAdjacentHTML("beforeend", $cardGameElement.repeat(numberOfCards));
---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function PlayerName(name) {
+	return `
+    <span class="player-name">
+        ${name}
+    </span>
+    `;
+}
+
+export default PlayerName;
